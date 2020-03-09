7502caeff25f6790bcb3a1c4c5be04dc76c5fc6e
jamalid <61936513+jamalid@users.noreply.github.com>
2020-03-09 23:23:25 +0700

    Update gojek.php

--- gojek.php
+++ gojek.php
@@ -79,7 +79,7 @@ echo color("white"," AUTO CREATE ACCOUNT GOJEK & AUTO REDEEM VOUCHER \n");
         sleep(1);
         }
         sleep(3);
-        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDE"}');
+        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDEPAY"}');
         $messageboba10 = fetch_value($boba10,'"message":"','"');
         if(strpos($boba10, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","(+) Message: ".$messageboba10);
